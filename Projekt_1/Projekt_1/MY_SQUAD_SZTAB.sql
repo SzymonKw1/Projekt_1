b1e6ae864469b8f037c9ab9f8fb9b850260b5b21
SzymonKw1 <skwiatkowski3@st.swps.edu.pl>
2024-10-26 13:28:17 +0200

    Dodano bazę danych SQL

---
+++ Projekt_1/Projekt_1/MY_SQUAD_SZTAB.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM my_squad.`sztab szkoleniowy`
+LIMIT 0, 1000
+
+-- Date: 2024-10-26 13:14
+*/
+INSERT INTO `` (`Imię i nazwisko`,`Funkcja`,`Staż pracy(w latach)`) VALUES ('Jose Mourinho','Trener główny',2);
+INSERT INTO `` (`Imię i nazwisko`,`Funkcja`,`Staż pracy(w latach)`) VALUES ('Fabrizio Romano','Rzecznik prasowy ',5);
+INSERT INTO `` (`Imię i nazwisko`,`Funkcja`,`Staż pracy(w latach)`) VALUES ('Tommy Angelo','Trener przygotowania motorycznego ',5);
+INSERT INTO `` (`Imię i nazwisko`,`Funkcja`,`Staż pracy(w latach)`) VALUES ('Paulie Lombardo','Trener przygotowania motorycznego',4);
+INSERT INTO `` (`Imię i nazwisko`,`Funkcja`,`Staż pracy(w latach)`) VALUES ('Vito Scaletta','Fizjoterapueta',2);
+INSERT INTO `` (`Imię i nazwisko`,`Funkcja`,`Staż pracy(w latach)`) VALUES ('Joe Barbaro','Fizjoterapueta',2);
+INSERT INTO `` (`Imię i nazwisko`,`Funkcja`,`Staż pracy(w latach)`) VALUES ('Mike Bruski','Trener Bramkarzy',7);
+INSERT INTO `` (`Imię i nazwisko`,`Funkcja`,`Staż pracy(w latach)`) VALUES ('Czesław Michniewicz','Asystent trenera ',2);
+INSERT INTO `` (`Imię i nazwisko`,`Funkcja`,`Staż pracy(w latach)`) VALUES ('Vincenzo Ricci','Trener przygotowania mentalnego',3);
+INSERT INTO `` (`Imię i nazwisko`,`Funkcja`,`Staż pracy(w latach)`) VALUES ('Carlo Ancelotti','Drugi trener',2);
---
+++ Projekt_1/Projekt_1/MY_SQUAD_ZAWODNICY.sql
@@ -0,0 +1,24 @@
+/*
+-- Query: SELECT * FROM my_squad.zawodnicy
+LIMIT 0, 1000
+
+-- Date: 2024-10-26 13:18
+*/
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Francesco Toldo','Bramkarz',1,39);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Ivan Cordoba','Obrońca',2,34);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Javier zanetti','Obrońca',4,37);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Dejan Stankovic','Środkowy pomocnik',5,32);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Lucimar da Silva Ferreira','Obrońca',6,33);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Samuel Eto\'o','Napastnik',9,30);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Weseley Snejider','Ofensywny pomocnik',10,27);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Sulley Muntari','Środkowy pomocnik',11,26);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Julio Cesar','Bramkarz',12,31);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Maicon','Obrońca',13,29);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('McDonald Mariga','Defensywny pomocnik',17,24);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Esteban Cambiasso','Defensywny pomocnik',19,30);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Diego Milito','Napastnik',22,32);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Marco Materazzi','Obrońca',23,37);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Walter Samuel','Obrońca',25,33);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Cristian Chivu','Obrońca',26,30);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Goran Pandev','Napastnik',27,27);
+INSERT INTO `` (`Imię i nazwisko`,`Pozycja`,`Numer`,`Wiek`) VALUES ('Mario Balotelli','Napastnik',45,19);
